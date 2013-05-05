035aabd97ccd5a490f0a2c026a7ffabc5b78241c
Brendan Moylan <jodimoylan@gmail.com>
2013-05-05 17:21:29 +0100

    Program added

---
+++ String_reverser/String_reverser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace String_reverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToBeReversed;
+            string newString = "";
+
+            Console.Write("Please input a string: ");
+            stringToBeReversed = Console.ReadLine();
+
+            for (int i = stringToBeReversed.Length; i > 0 ; i--)
+            {
+                newString += Convert.ToString(stringToBeReversed[i-1]);                
+            }
+
+            Console.WriteLine("Your reversed string is: " + newString);
+
+            Console.ReadKey();
+        }
+    }
+}
